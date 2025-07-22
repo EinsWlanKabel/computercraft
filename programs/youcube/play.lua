79d609f5f6906f536a1c70b22dae323fea7458b8
EinsWlanKabel <58028347+EinsWlanKabel@users.noreply.github.com>
2025-07-22 14:11:24 +0200

    Update play.lua

--- programs/youcube/play.lua
+++ programs/youcube/play.lua
@@ -1,13 +1,32 @@
 local arg1 = { ... }
 local default = ""
-if #arg1 > 0 then
-    term.clear()
-    term.setCursorPos(1,1)
-    shell.run("youcube --lp --sh --nv "..table.concat(arg1, " "))
-elseif default ~= "" then
-    shell.run("youcube --lp --sh --nv "..default)
-else
-    print("Usage:")
-    print("play <youtube link>")
-    print("play <youtube search terms>")
+local textScale = 1
+if pocket then
+    if #arg1 > 0 then
+        term.clear()
+        term.setCursorPos(1,1)
+        shell.run("youcube --lp --sh --nv "..table.concat(arg1, " "))
+    elseif default ~= "" then
+        shell.run("youcube --lp --sh --nv "..default)
+    else
+        print("Usage:")
+        print("play <youtube link>")
+        print("play <youtube search terms>")
+    end
+elseif #arg1 > 0 then
+    m = peripheral.find(arg1[1])
+    if not m or peripheral.getType(m) ~= "monitor" then
+        error(arg1[1] .. " is not a valid monitor name.")
+    end
+    table.remove(arg1, 1)
+    m.setTextScale(textScale)
+    if arg1 > 0 then
+        shell.run("monitor right youcube "..table.concat(arg1, " "))
+    else default ~= "" then
+        shell.run("monitor right youcube "..default)
+    else
+        print("Usage:")
+        print("play <monitor name> <youtube link>")
+        print("play <monitor name> <youtube search terms>")
+    end
 end
