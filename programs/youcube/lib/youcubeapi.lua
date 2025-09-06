afc9ffbc81a539a29f16b1952995c2bdb1aa77dc
EinsWlanKabel <58028347+EinsWlanKabel@users.noreply.github.com>
2025-09-06 03:15:15 +0200

    Update youcubeapi.lua

--- programs/youcube/lib/youcubeapi.lua
+++ programs/youcube/lib/youcubeapi.lua
@@ -32,7 +32,10 @@ end
 -- Look at the [Documentation](https://commandcracker.github.io/YouCube/) for moor information
 -- Contact the server owner on Discord, when the server is down
 local servers = {
-    "ws://172.18.0.4:5000", -- Your server!
+    "ws://172.18.0.2:5000",
+    "ws://172.18.0.3:5000",
+    "ws://172.18.0.4:5000",
+    "ws://172.18.0.5:5000", -- Your server!
     --"wss://youcube.knijn.one", -- By EmmaKnijn#0043
     --"wss://youcube.onrender.com", -- By Commandcracker#8528
 }
